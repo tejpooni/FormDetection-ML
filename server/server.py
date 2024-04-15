4170676f3074e95a9c1dc58f0e49910505442088
Tej Pooni <tejpooni@Tejs-MBP-2.lan>
2024-04-15 00:04:48 -0700

    Revert "cross platform pathing"
    This reverts commit 8643539d866ee347b2a74b14ff0e18fe56295af1.

--- server/server.py
+++ server/server.py
@@ -6,13 +6,14 @@ from flask import send_file as sf
 from werkzeug.utils import secure_filename
 import os
 
-
 # app init 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 app.config['UPLOAD_FOLDER'] = 'uploads/'  # Directory where files will be stored
 CORS(app,resources={r"/*":{"origins":"*"}})
 socketio = SocketIO(app,cors_allowed_origins="*")
+
+
 
 @app.route('/processed_video/<filename>')
 def processed_video(filename):
@@ -57,11 +58,11 @@ def send_file():
 
 @app.route('/get_vid', methods=["GET"])
 def get_vid():  
-    return sf(os.path.join('outputs', 'output.mp4'), mimetype='video/mp4', as_attachment=True)
+    return sf("outputs\output.mp4", mimetype='video/mp4', as_attachment=True)
 
 @app.route('/get_feedback', methods=["GET"])
 def get_feedback():
-    return sf(os.path.join('outputs', 'feedback.json'))
+    return sf("outputs\\feedback.json")
 
 @socketio.on("connect")
 def connected():
